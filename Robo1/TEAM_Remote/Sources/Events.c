20d5ca168f43785b0a80d8a524062dfc9b1c2227
Thomas Kempf <thomas.kempf@stud.hslu.ch>
2017-10-26 14:29:45 +0200

    Interrupt on Remote buttons

--- Robo1/TEAM_Remote/Sources/Events.c
+++ Robo1/TEAM_Remote/Sources/Events.c
@@ -36,6 +36,7 @@ extern "C" {
 
 /* User includes (#include below this line is not maintained by Processor Expert) */
 #include "Timer.h"
+#include "Keys.h"
 /*
 ** ===================================================================
 **     Event       :  Cpu_OnNMIINT (module Events)
@@ -73,6 +74,96 @@ void TI1_OnInterrupt(void)
 {
   /* Write your code here ... */
   TMR_OnInterrupt();
+}
+
+/*
+** ===================================================================
+**     Event       :  SW5_OnInterrupt (module Events)
+**
+**     Component   :  SW5 [ExtInt]
+**     Description :
+**         This event is called when an active signal edge/level has
+**         occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void SW5_OnInterrupt(void)
+{
+  /* Write your code here ... */
+	KEY_OnInterrupt(KEY_BTN5);
+}
+
+/*
+** ===================================================================
+**     Event       :  SW4_OnInterrupt (module Events)
+**
+**     Component   :  SW4 [ExtInt]
+**     Description :
+**         This event is called when an active signal edge/level has
+**         occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void SW4_OnInterrupt(void)
+{
+  /* Write your code here ... */
+	KEY_OnInterrupt(KEY_BTN4);
+}
+
+/*
+** ===================================================================
+**     Event       :  SW3_OnInterrupt (module Events)
+**
+**     Component   :  SW3 [ExtInt]
+**     Description :
+**         This event is called when an active signal edge/level has
+**         occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void SW3_OnInterrupt(void)
+{
+  /* Write your code here ... */
+	KEY_OnInterrupt(KEY_BTN3);
+}
+
+/*
+** ===================================================================
+**     Event       :  SW2_OnInterrupt (module Events)
+**
+**     Component   :  SW2 [ExtInt]
+**     Description :
+**         This event is called when an active signal edge/level has
+**         occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void SW2_OnInterrupt(void)
+{
+  /* Write your code here ... */
+	KEY_OnInterrupt(KEY_BTN2);
+}
+
+/*
+** ===================================================================
+**     Event       :  SW1_OnInterrupt (module Events)
+**
+**     Component   :  SW1 [ExtInt]
+**     Description :
+**         This event is called when an active signal edge/level has
+**         occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void SW1_OnInterrupt(void)
+{
+  /* Write your code here ... */
+	KEY_OnInterrupt(KEY_BTN1);
 }
 
 /* END Events */
